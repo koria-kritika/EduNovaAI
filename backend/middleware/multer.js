b1eef97b1567f365da0156a9b98d65b7b70f7eee
Kritika Koria <kritikakoria.600202@gmail.com>
2026-05-26 02:50:34 +0530

    fix multer upload path for render

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,14 +1,14 @@
 import multer from "multer"
 
 let storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null , "./public")
+    destination: (req, file, cb) => {
+        cb(null, "/tmp")
     },
-    filename:(req,file,cb)=>{
-        cb(null , file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "_" + file.originalname)
     }
-
 })
-const upload = multer({storage})
+
+const upload = multer({ storage })
 
 export default upload
